f810817f78caabc61ef66824f3e29c6572762ad0
ssande-fwd <ssande@forwardedu.com>
2025-05-23 14:08:24 -0400

    pump tests

--- fwd-breakout/test.ts
+++ fwd-breakout/test.ts
@@ -1,1 +1,14 @@
+// pump tests
+// isOn(): boolean
+// setActive(state: boolean): void
+// timedRun(duration: number): void
+// setActive() is used in timedRun, so it is covered by timedRun() test
 console.log("Pump on? " + motors.pump.isOn())
+console.log("Run pump for 3s.")
+motors.pump.timedRun(3)
+basic.pause(1000)
+console.log("1s... Pump on? " + motors.pump.isOn())
+basic.pause(1000)
+console.log("2s... Pump on? " + motors.pump.isOn())
+basic.pause(1000)
+console.log("3s... Pump on? " + motors.pump.isOn())
