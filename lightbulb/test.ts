f71e2487fb1ce3d2a1eab6330a11721ec0a37021
ssande-fwd <ssande@forwardedu.com>
2025-05-26 08:28:41 -0400

    lightbulb test

--- lightbulb/test.ts
+++ lightbulb/test.ts
@@ -1,1 +1,19 @@
-modules.lightbulb1.setBrightness(100)
+basic.forever(() => {
+    console.log("Test Start")
+    console.log(
+        "The brightness is changing every second for 6 seconds, but the Jacdac simulator only shows on / off."
+    )
+    basic.pause(1000)
+    modules.lightbulb1.setBrightness(100)
+    basic.pause(1000)
+    modules.lightbulb1.setBrightness(75)
+    basic.pause(1000)
+    modules.lightbulb1.setBrightness(50)
+    basic.pause(1000)
+    modules.lightbulb1.setBrightness(25)
+    basic.pause(1000)
+    modules.lightbulb1.setBrightness(10)
+    basic.pause(1000)
+    modules.lightbulb1.setBrightness(0)
+    console.log("Test End")
+})
