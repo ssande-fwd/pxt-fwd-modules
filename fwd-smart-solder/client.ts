54a971d2e56d63f0580b8ecd2ebd811c80b02e7c
ssande-fwd <ssande@forwardedu.com>
2025-05-27 09:14:45 -0400

    solder rename

--- fwd-smart-solder/client.ts
+++ fwd-smart-solder/client.ts
@@ -1,6 +1,6 @@
 namespace buttons {
     //% fixedInstances
-    export class FwdLTSBtnClient extends modules.ButtonClient {
+    export class FwdSolderBtnClient extends modules.ButtonClient {
         constructor(role: string) {
             super(role)
         }
@@ -9,9 +9,9 @@ namespace buttons {
          * Code to run when a chosen event occurs
          * @param event Button pressed (down), held, released (up)
          */
-        //% group="Learn To Solder"
+        //% group="Smart Solder"
         //% block="on $this $event"
-        //% blockId=fwd_lts_on_event
+        //% blockId=fwd_solderbtn_on_event
         onEvent(event: jacdac.ButtonEvent, handler: () => void) {
             super.onEvent(event, handler)
         }
@@ -19,9 +19,9 @@ namespace buttons {
         /**
          * Returns the ms duration of the last button hold in ms
          */
-        //% group="Learn To Solder"
+        //% group="Smart Solder"
         //% block="$this hold duration (ms)"
-        //% blockId=fwd_lts_hold_duration
+        //% blockId=fwd_solderbtn_hold_duration
         holdDuration(): number {
             return super.holdDuration()
         }
@@ -29,32 +29,32 @@ namespace buttons {
         /**
          * Returns true if the button is currently pressed, otherwise false
          */
-        //% group="Learn To Solder"
+        //% group="Smart Solder"
         //% block="$this pressed"
-        //% blockId=fwd_lts_is_pressed
+        //% blockId=fwd_solderbtn_is_pressed
         isPressed(): boolean {
             return super.pressed()
         }
     }
 
     //% fixedInstance whenUsed
-    export const BTN1 = new FwdLTSBtnClient("BTN1?srvo=0")
+    export const BTN1 = new FwdSolderBtnClient("BTN1?srvo=0")
     //% fixedInstance whenUsed
-    export const BTN2 = new FwdLTSBtnClient("BTN2?srvo=1")
+    export const BTN2 = new FwdSolderBtnClient("BTN2?srvo=1")
     //% fixedInstance whenUsed
-    export const BTN3 = new FwdLTSBtnClient("BTN3?srvo=2")
+    export const BTN3 = new FwdSolderBtnClient("BTN3?srvo=2")
 
     //% fixedInstance whenUsed
-    export const BTN4 = new FwdLTSBtnClient("BTN4")
+    export const BTN4 = new FwdSolderBtnClient("BTN4")
     //% fixedInstance whenUsed
-    export const BTN5 = new FwdLTSBtnClient("BTN5")
+    export const BTN5 = new FwdSolderBtnClient("BTN5")
     //% fixedInstance whenUsed
-    export const BTN6 = new FwdLTSBtnClient("BTN6")
+    export const BTN6 = new FwdSolderBtnClient("BTN6")
 }
 
 namespace lights {
     //% fixedInstances
-    export class FwdLTSLightClient extends modules.LightbulbClient {
+    export class FwdSolderLightClient extends modules.LightbulbClient {
         constructor(role: string) {
             super(role)
         }
@@ -63,8 +63,8 @@ namespace lights {
          * Indicates the brightness of the light bulb. Zero means completely off and 0xffff means completely on.
          * For non-dimmable lights, the value should be clamp to 0xffff for any non-zero value.
          */
-        //% group="Learn To Solder"
-        //% blockId=jacdac_ltslight_brightness___set
+        //% group="Smart Solder"
+        //% blockId=fwd_solderlight_set_brightness
         //% block="set %lights brightness to %value (\\%)"
         //% weight=100
         //% value.min=0
@@ -76,16 +76,16 @@ namespace lights {
     }
 
     //% fixedInstance
-    export const GREEN = new FwdLTSLightClient("GREEN?srvo=0")
+    export const GREEN = new FwdSolderLightClient("GREEN?srvo=0")
     //% fixedInstance
-    export const YELLOW = new FwdLTSLightClient("YELLOW?srvo=1")
+    export const YELLOW = new FwdSolderLightClient("YELLOW?srvo=1")
     //% fixedInstance
-    export const RED = new FwdLTSLightClient("RED?srvo=2")
+    export const RED = new FwdSolderLightClient("RED?srvo=2")
 
     //% fixedInstance
-    export const GREEN2 = new FwdLTSLightClient("GREEN2")
+    export const GREEN2 = new FwdSolderLightClient("GREEN2")
     //% fixedInstance
-    export const YELLOW2 = new FwdLTSLightClient("YELLOW2")
+    export const YELLOW2 = new FwdSolderLightClient("YELLOW2")
     //% fixedInstance
-    export const RED2 = new FwdLTSLightClient("RED2")
+    export const RED2 = new FwdSolderLightClient("RED2")
 }
